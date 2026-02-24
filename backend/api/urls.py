245cfc9a6eb4fb72eb85634f39891b6117eaf5ee
aquijan <166669724+aquijan@users.noreply.github.com>
2026-02-24 01:40:02 -0700

    Update urls.py
    added path for leaderboard

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("api/balance/", views.coinbalance, name="balance"),
     path("api/awardCoins/", views.awardCoins, name="awardCoins"),
     path("api/transaction/", views.transaction, name="transaction"),
+    path("api/leaderboard/", views.leaderboard, name="leaderboard"),
 
     path("api/register_student/", views.register_student, name="register_student"),
     path("api/register_instructor/", views.register_instructor, name="register_instructor"),
